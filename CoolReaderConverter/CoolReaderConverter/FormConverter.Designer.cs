5fc85883710078a18773dd2e315ddbd317fa115f
Nick <nick.kuzmin.developer@gmail.com>
2014-07-23 11:24:23 +0300

    Converter

---
+++ CoolReaderConverter/CoolReaderConverter/FormConverter.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CoolReaderConverter
+{
+    partial class FormConverter
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormConverter));
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(13, 13);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // FormConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 108);
+            this.Controls.Add(this.openFileButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormConverter";
+            this.Text = "Cool Reader Converter Bookmarks";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileButton;
+    }
+}
+
---
+++ CoolReaderConverter/CoolReaderConverter/FormConverter.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CoolReaderConverter
+{
+    public partial class FormConverter : Form
+    {
+        public FormConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog;
+            Stream myStream;
+            if (!IsSelectedCorrectFile(out openFileDialog, out myStream)) return;
+
+            using (myStream)
+            {
+                using (var sr = File.OpenText(openFileDialog.FileName))
+                {
+                    List<string> bookmarksList;
+                    if (GetBookmarks(sr, out bookmarksList)) return;
+
+                    DysplayBookmarksInCorrectFormat(bookmarksList);
+                    SaveBookmarksInTextFile(bookmarksList);
+                }
+            }
+        }
+
+        private static void SaveBookmarksInTextFile(List<string> bookmarksList)
+        {
+            Stream myStream;
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            var pathSaving = String.Empty;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog.OpenFile()) != null)
+                {
+                    pathSaving = saveFileDialog.FileName;
+                    Console.WriteLine(pathSaving);
+                    myStream.Close();
+                }
+            }
+
+            using (var file = new StreamWriter(pathSaving))
+            {
+                foreach (var bookmark in bookmarksList)
+                {
+                    file.WriteLine(bookmark);
+                }
+            }
+        }
+
+        private static void DysplayBookmarksInCorrectFormat(List<string> bookmarksList)
+        {
+            foreach (var bookmark in bookmarksList)
+            {
+                Console.WriteLine(bookmark);
+            }
+        }
+
+        private static bool GetBookmarks(StreamReader sr, out List<string> bookmarksList)
+        {
+            var fullText = sr.ReadToEnd();
+            Console.WriteLine(fullText);
+            bookmarksList = new List<string>();
+            const string symbolBeginingBookmark = "<<";
+            const string symbolEndingBookmark = ">>";
+            while (fullText.Contains(symbolBeginingBookmark))
+            {
+                try
+                {
+                    var currentBookmark = String.Empty;
+                    var indexSymbolBegningBookmarks = fullText.IndexOf(symbolBeginingBookmark);
+                    var indexSymbolEndingBookmarks = fullText.IndexOf(symbolEndingBookmark);
+                    var lengthCurrentBookmark = indexSymbolEndingBookmarks - indexSymbolBegningBookmarks;
+                    currentBookmark = fullText.Substring(indexSymbolBegningBookmarks + 2, lengthCurrentBookmark - 2);
+                    fullText = fullText.Remove(0, indexSymbolEndingBookmarks + 5);
+
+                    bookmarksList.Add("- " + currentBookmark);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Some Exception on loop while!");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool IsSelectedCorrectFile(out OpenFileDialog openFileDialog, out Stream myStream)
+        {
+            openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = Application.ExecutablePath,
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                myStream = null;
+                return false;
+            }
+
+            myStream = null;
+            try
+            {
+                if ((myStream = openFileDialog.OpenFile()) != null)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+            }
+            return false;
+        }
+    }
+}
